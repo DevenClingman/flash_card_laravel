d8ff6ebb6f8b938dfe2385bfa44cc6e29b3448e0
Deven <dclingy@gmail.com>
2021-06-08 13:05:46 -0600

    add one to many relationship for card and deck tables

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Deck;
 
 class Card extends Model
 {
     use HasFactory;
 
     public function deck(){
-        
+        return $this->belongsTo(Deck::class);
     }
 }
--- app/Models/Deck.php
+++ app/Models/Deck.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Card;
 
 class Deck extends Model
 {
     use HasFactory;
+
+    public function cards(){
+        return $this->hasMany(Card::class);
+    }
 }
