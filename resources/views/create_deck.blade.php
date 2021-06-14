a8f6540f9bc89e8eb5d62256cf6ed032eb55bf1f
Deven <dclingy@gmail.com>
2021-06-13 20:35:08 -0600

    added card observer

--- resources/views/create_deck.blade.php
+++ resources/views/create_deck.blade.php
@@ -3,7 +3,7 @@
 @section('content')
    <div class="container" style="padding:5%;" >
       <form method="post" action="{{ route('decks.store') }}">
-         <div style="background-color:gray; opacity:0.9; padding:5%;" class="form-group">
+         <div style="background-color:gray; opacity:0.7; padding:5%;" class="form-group">
             @csrf
             <input class="form-control" name="name" type="text" placeholder="Deck Name"></input>
             <br><br>
