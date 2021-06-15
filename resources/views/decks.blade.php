028c2e17023c69f93e3c437e32be016b43bdd240
Deven <dclingy@gmail.com>
2021-06-14 22:42:29 -0600

    make changes to decks view

--- resources/views/decks.blade.php
+++ resources/views/decks.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>All Decks</h3>
-    <ul>
-        @foreach($decks as $deck)
-            <li>
-                <a href="{{ route('decks.show', ['deck' => $deck->id]) }}">{{$deck->name}}--</a>
-                <a href="{{ route('decks.edit', ['deck' => $deck->id]) }}">Edit / </a>
-                <a href="{{ route('decks.destroy', ['deck' => $deck->id]) }}">Delete</a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <h3>All Decks</h3>
+        <div>
+            <ul class="list-group">
+            @foreach($decks as $deck)
+                <li class="list-group-item list-group-item-action">
+                    <a href="{{ route('decks.show', ['deck' => $deck->id]) }}">{{$deck->name}}--</a>
+                    <a href="{{ route('decks.edit', ['deck' => $deck->id]) }}">Edit / </a>
+                    <a href="{{ route('decks.destroy', ['deck' => $deck->id]) }}">Delete</a>
+                </li>
+            @endforeach
+            </ul>
+        </div>
+    </div>
 @endsection
