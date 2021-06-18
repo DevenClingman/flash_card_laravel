f4fe7c47b4e3431ccd624b69635735d484c57071
Deven <dclingy@gmail.com>
2021-06-18 11:03:19 -0600

    refactor stylesheets to be DRY

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,10 @@
     <div style="text-align:center">
         <h1>Welcome to the Flash Card App!</h1>
 
-        <a class="btn btn-outline-dark btn-lg" style="color:white" href="{{ route('decks.create') }}">Lets Get Started</a>
+        <a class="btn btn-outline-dark btn-lg home-page-btns" 
+           href="{{ route('decks.create') }}">Lets Get Started</a>
         <br><br>
-        <a class="btn btn-outline-dark btn-lg" 
-           style="color:white"
-           style="background:rgba(0, 0, 0, 1)" 
+        <a class="btn btn-outline-dark btn-lg home-page-btns"  
            href="{{ route('decks.index') }}">Let Me See My Decks</a>
     </div>
 </div>
--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -1,5 +1,6 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/design.css') }}" rel="stylesheet">  
     <link href="{{ asset('css/backgroundVideo.css') }}" rel="stylesheet">
