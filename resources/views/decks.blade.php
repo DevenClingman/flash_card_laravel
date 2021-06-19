4e11ae3a984cc3bebd9b79a22d389fc312fad309
Deven <dclingy@gmail.com>
2021-06-18 20:39:44 -0600

    update styles of decks page

--- resources/views/decks.blade.php
+++ resources/views/decks.blade.php
@@ -2,14 +2,21 @@
 
 @section('content')
     <div class="container">
-        <h3>All Decks</h3>
         <div>
             <ul class="list-group">
             @foreach($decks as $deck)
-                <li class="list-group-item list-group-item-action">
-                    <a href="{{ route('decks.show', ['deck' => $deck->id]) }}">{{$deck->name}}--</a>
-                    <a href="{{ route('decks.edit', ['deck' => $deck->id]) }}">Edit / </a>
-                    <a href="{{ route('decks.destroy', ['deck' => $deck->id]) }}">Delete</a>
+                <li class="list-group-item list-group-item-action">   
+                    <a href="{{ route('decks.show', ['deck' => $deck->id]) }}">
+                        <div class="row">
+                            <div class="col-md">
+                                <a href="{{ route('decks.show', ['deck' => $deck->id]) }}"><h4>{{$deck->name}}</h4></a> 
+                            </div>
+                            <div class="col-md" style="text-align:right">
+                                <a href="{{ route('decks.edit', ['deck' => $deck->id]) }}">Edit / </a>
+                                <a href="{{ route('decks.destroy', ['deck' => $deck->id]) }}">Delete</a>
+                            </div>
+                        </div>
+                    </a> 
                 </li>
             @endforeach
             </ul>
