1e465b7be924bdeb693618afaf07374c6c39ffb5
Deven <dclingy@gmail.com>
2021-06-29 12:35:28 -0600

    update decks view

--- resources/views/decks.blade.php
+++ resources/views/decks.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container" style="padding:5%;">
         <div>
             <ul class="list-group">
             @foreach($decks as $deck)
-                <li class="list-group-item list-group-item-action">   
+                <li style="background-color:gray;" class="list-group-item list-group-item-action">   
                     <a href="{{ route('decks.show', ['deck' => $deck->id]) }}">
                         <div class="row">
                             <div class="col-md">
-                                <a href="{{ route('decks.show', ['deck' => $deck->id]) }}"><h4>{{$deck->name}}</h4></a> 
+                                <a class="text-white"href="{{ route('decks.show', ['deck' => $deck->id]) }}"><h4>{{$deck->name}}</h4></a> 
                             </div>
                             <div class="col-md" style="text-align:right">
-                                <a href="{{ route('decks.edit', ['deck' => $deck->id]) }}">Edit / </a>
-                                <a href="{{ route('decks.destroy', ['deck' => $deck->id]) }}">Delete</a>
+                                <a class="btn btn-info" href="{{ route('decks.edit', ['deck' => $deck->id]) }}">Edit</a>
+                                <a class="btn btn-danger" href="{{ route('decks.destroy', ['deck' => $deck->id]) }}">Delete</a>
                             </div>
                         </div>
                     </a> 
