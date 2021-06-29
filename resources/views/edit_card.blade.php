d8442eef6dfbd01d6b82ae75d2c0b5fcdcc11118
Deven <dclingy@gmail.com>
2021-06-29 12:17:56 -0600

    update edit card view

--- resources/views/edit_card.blade.php
+++ resources/views/edit_card.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to the Edit Card Page!</h1>
-
-    <form method="post" action="{{ route('cards.update', ['card'=> $card_id]) }}">
-        @csrf
-        <textarea name="question" type="text">{{ $card->question }}</textarea>
-        <br><br>
-        <textarea name="answer" type="text">{{ $card->answer }}</textarea>
-        <br><br>
-        <button type="submit">Submit</button>
-   </form>
+    <div class="container" style="padding:5%;">
+        <form method="post" action="{{ route('cards.update', ['card'=> $card_id]) }}">
+            <div class="form-group" style="background-color:gray; padding:5%; opacity:0.7;">
+                @csrf
+                <textarea class="form-control" name="question" type="text">{{ $card->question }}</textarea>
+                <br><br>
+                <textarea class="form-control" name="answer" type="text">{{ $card->answer }}</textarea>
+                <br><br>
+                <button class="btn btn-dark btn-block" type="submit">Submit</button>
+            </div>
+       </form>
+    </div>
+    
 
 @endsection
