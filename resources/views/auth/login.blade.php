13cab422ef927f805ce44d654786ca975b93b71a
Deven <dclingy@gmail.com>
2021-07-05 16:13:41 -0600

    fix errors to allow for registration of user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,3 @@
-<x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -53,4 +52,3 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
