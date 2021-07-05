280c5c22e87e99554f849b3a705ea22062a507a5
Deven <dclingy@gmail.com>
2021-07-05 11:58:26 -0600

    added breeze auth and removed some ui and styling

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,9 @@ Route::get('/cards/delete/{card}', [CardsController::class, 'destroy'])->name('c
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
